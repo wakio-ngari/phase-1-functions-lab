939f82ae4b40ea3349808ca14bec2e3aef12a754
wakio-ngari <ngarijoyce017@gmail.com>
2024-11-27 19:45:30 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+
+let distanceFromHqInBlocks = someValue;
+ function distanceFromHqInBlocks(block) {
+    const hqBlock = 42;  
+    return Math.abs(hqBlock - block);
+  }
+  function distanceFromHqInFeet(block) {
+    const blocks = distanceFromHqInBlocks(block);
+    return blocks * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(start - destination);
+    return distanceInBlocks * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    
+
+    if (distanceInFeet <= 400) {
+      return 0;
+    }
+    
+     if (distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;
+    }
+    
+      if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+    }
+    
+    return 'cannot travel that far';
+  }
+  
+  
+
+  console.log(distanceFromHqInBlocks(50));  
+  
+  console.log(distanceTravelledInFeet(5, 10));  
+  
+  console.log(calculatesFarePrice(5, 10));  
+  
